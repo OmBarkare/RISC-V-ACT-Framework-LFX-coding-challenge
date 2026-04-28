cc2020126928ec17793b81ef8a3a3aed2248ef92
omomo <om062006@gmail.com>
2026-04-28 22:59:00 +0530

    init

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int main() {
+    int fd = open("/dev/ttyS0", O_RDWR);
+
+    if(fd < 0) {
+        fprintf(stderr, "unable to open /dev/ttsy0");
+    }
+
+    struct termios t;
+
+    // copy current configuration to tty
+    tcgetattr(fd, &t);
+
+    // disbale translation and flow control
+    // disapble post processing
+    // disable canonical mode, signals, echo, extensions
+    // disable parity and set data size to 8 bit
+    // set VMIN = 1, VTIME = 0
+    cfmakeraw(&t);
+
+    // for non-blocking behaviour
+    t.c_cc[VMIN] = 0;
+}
