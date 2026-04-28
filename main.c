18774130e2513dcea4060f1935d6a2f7503c7b46
omomo <om062006@gmail.com>
2026-04-28 23:16:00 +0530

    refactor
    move config functionality to tty_config

--- main.c
+++ main.c
@@ -5,25 +5,31 @@
 int main() {
     int fd = open("/dev/ttyS0", O_RDWR);
 
+    struct termios t;
+    tty_config(&t, fd);
+}
+
+
+void tty_config(struct termios *t, int fd) {
+
     if(fd < 0) {
-        fprintf(stderr, "unable to open /dev/ttsy0");
+        perror("unable to open /dev/ttsy0");
+        return -1;
     }
 
-    struct termios t;
-
     // copy current configuration to tty
-    tcgetattr(fd, &t);
+    tcgetattr(fd, t);
 
     // disbale translation and flow control
     // disapble post processing
     // disable canonical mode, signals, echo, extensions
     // disable parity and set data size to 8 bit
     // set VMIN = 1, VTIME = 0
-    cfmakeraw(&t);
+    cfmakeraw(t);
 
     // for non-blocking behaviour
-    t.c_cc[VMIN] = 0;
+    t->c_cc[VMIN] = 0;
 
     // clear stop bit to have 1 stob bit
-    t.c_cflag &= ~(CSTOPB);
+    t->c_cflag &= ~(CSTOPB);
 }
