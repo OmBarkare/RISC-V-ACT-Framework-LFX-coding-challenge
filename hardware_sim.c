336d7f6b595e753a69dfbb51f3f1d7a68c00ea35
omomo <om062006@gmail.com>
2026-05-04 12:00:24 +0530

    fix: add error handling and tty config

--- hardware_sim.c
+++ hardware_sim.c
@@ -3,9 +3,40 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <termios.h>
+
+int tty_config(struct termios *t);
 
 int main() {
-    int fd = open("/tmp/ttyHw", O_RDWR);
+    int fd = open("/tmp/ttyHw", O_RDWR | O_NOCTTY);
+    if (fd < 0) {
+        perror("/tmp/ttyHw");
+        return -1;
+    }
+
+    struct termios t;
+
+    // copy current configuration to tty
+    if (tcgetattr(fd, &t) < 0) {
+        perror("tcgetattr");
+        close(fd);
+        return -1;
+    }
+
+    // apply required config over copied config
+    if (tty_config(&t) < 0) {
+        fprintf(stderr, "tty_config failed\n");
+        close(fd);
+        return -1;
+    }
+
+    // set config 
+    if (tcsetattr(fd, TCSANOW, &t) < 0) {
+        perror("tcsetattr");
+        close(fd);
+        return -1;
+    }
+
 
     struct pollfd p;
     p.fd = fd;
@@ -16,15 +47,27 @@ int main() {
 
         if (ret < 0) {
             perror("poll error");
-            break;
+            break; // <--------------- do i return?
         } else if (ret == 0) {
             printf("no mssg from laptop\n");
-            write(fd, "ARE U THERE", 11);
+            if (write(fd, "ARE U THERE", 11) < 0) {
+                perror("write failed");
+                // <----------------- do I return?
+            }
         }
 
         if (p.revents & POLLIN) {
             char recv_buf[100];
             int n = read(fd, recv_buf, 99);
+            if(n < 0) {
+                perror("read failed");
+                close(fd);
+                return -1;
+            }
+            else if(n == 0) {
+                printf("read EOF\n");
+                break;
+            }
             recv_buf[n] = '\0';
             
             if (strcmp(recv_buf, "ILU TOO") == 0) {
@@ -38,9 +81,44 @@ int main() {
                 }
                 printf("mssg received: %s,\nsending response: %s\n", recv_buf, "OK");
             } else {
-                write(fd, "ILU", 3);
+                if (write(fd, "ILU", 3) < 0) {
+                    perror("could not write");
+                }
                 printf("mssg received: %s,\nsending response: %s\n", recv_buf, "ILU");
             }
         }
     }
+
+    close(fd);
+    return 0;
+}
+
+int tty_config(struct termios *t) {
+
+    // disable canonical mode, signals, echo, extensions
+    // disable parity and set data size to 8 bit
+    // set VMIN = 1, VTIME = 0
+    cfmakeraw(t);
+
+    // override VMIN = 1 for non-blocking behaviour,
+    // read() returns even if 0 bytes are read
+    t->c_cc[VMIN] = 0;
+
+    // clear stop bit flag to have only 1 terminating stop bit
+    t->c_cflag &= ~(CSTOPB);
+
+    // set baudrate
+    if (cfsetispeed(t, B115200) < 0) {
+        perror("cfsetispeed");
+        return -1;
+    }
+    if (cfsetospeed(t, B115200) < 0) {
+        perror("cfsetospeed");
+        return -1;
+    }
+
+    // enable receiver and ignore modem control signals
+    t->c_cflag |= (CREAD | CLOCAL);
+
+    return 0;
 }
